44c34b63af3f21909bcc4acfc022e06f530a7369
000saddamahmad <000saddamahmad@gmail.com>
2025-12-19 06:24:17 -0800

    Refactor mobile menu and add accordion dropdown
    Refactor menu toggle and add accordion functionality for mobile.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,25 @@
-const toggle = document.getElementById('menuToggle');
-const menu = document.getElementById('mainMenu');
+const menuToggle = document.getElementById("menuToggle");
+const mainMenu = document.getElementById("mainMenu");
+const dropdownTitles = document.querySelectorAll(".dropdown-title");
 
-toggle.addEventListener('click', () => {
-  menu.classList.toggle('show');
+// Mobile menu toggle
+menuToggle.addEventListener("click", () => {
+  mainMenu.classList.toggle("show");
+});
+
+// Accordion dropdown (mobile)
+dropdownTitles.forEach(title => {
+  title.addEventListener("click", () => {
+
+    dropdownTitles.forEach(t => {
+      if (t !== title) {
+        t.nextElementSibling.style.display = "none";
+      }
+    });
+
+    const content = title.nextElementSibling;
+    content.style.display =
+      content.style.display === "block" ? "none" : "block";
+  });
 });
 
